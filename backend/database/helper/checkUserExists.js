3ce29b9afada5dd71a013f6a1e9bfd61ee734cb7
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-04-27 11:30:01 -0300

    CHANGE: Modularized user controllers!

---
+++ backend/database/helper/checkUserExists.js
@@ -0,0 +1,13 @@
+const { User } = require("../mongoose")
+
+//Funcion para verificar si el usuario existe en la base de datos
+
+const checkUserExists = async (idUser) => {
+    const isUser = await User.findById(idUser)
+    if (idUser !== null) {
+        return true
+    }
+    else return false
+}
+
+module.exports = checkUserExists
