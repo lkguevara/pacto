64e22379b75622123aa6ce407e3db5cc3ddf0d69
sumerlost <Gaby_98@hotmail.com.ar>
2023-04-27 17:31:43 -0300

    DBProductGetCategory created

--- backend/database/controllers/DBProductGetId.js
+++
@@ -1,8 +0,0 @@
-const Product = require("../models/product");
-
-const getProductId = async (idProduct) => {
-  const productFind = await Product.findOne({ _id: idProduct });
-  return productFind;
-};
-
-module.exports = getProductId;
