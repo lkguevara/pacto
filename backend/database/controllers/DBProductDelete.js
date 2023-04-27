1a2b02396987074564df1210f87799160c4ff2d0
sumerlost <Gaby_98@hotmail.com.ar>
2023-04-27 16:13:19 -0300

    DBProductGetId created

--- backend/database/controllers/DBProductDelete.js
+++ backend/database/controllers/DBProductDelete.js
@@ -1,9 +1,9 @@
-const Product = require("../../database/models/product");
+const Product = require("../models/product");
 
 const deleteProduct = async (idProduct) => {
-    const porductUpdate = await Product.update({ _id: idProduct },{active:false});
-  
-    return porductUpdate;
-  };
-  
-  module.exports = deleteProduct;
+  const porductUpdate = await Product.update({ _id: idProduct }, { active: false });
+
+  return porductUpdate;
+};
+
+module.exports = deleteProduct;
