92bb65179c3606f2271b62debcf240188e942962
sumerlost <Gaby_98@hotmail.com.ar>
2023-04-27 11:25:38 -0300

    file name and function fixed

--- backend/database/helper/checkUserExists.js
+++ backend/database/helper/checkUserExists.js
@@ -2,7 +2,7 @@ const { User } = require("../mongoose")
 
 //Funcion para verificar si el usuario existe en la base de datos
 
-const checkUserExist = async (idUser) => {
+const checkUserExists = async (idUser) => {
     const isUser = await User.findById(idUser)
     if (idUser !== null) {
         return true
