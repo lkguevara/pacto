d32850e3c835c00d7c1f306dbbff8c8ee9a3dc71
Alexbv2615 <alexbv2615@gmail.com>
2023-04-27 12:54:27 -0500

    modificacion alt de Imagen

--- client/src/components/enConstruccion.js
+++ client/src/components/enConstruccion.js
@@ -7,7 +7,7 @@ const EnConstruccion = () => {
         <div className={styles.container}>
             <h1>Página en Construcción</h1>
             <div className={styles.imgContainer}>
-            <Image priority src={ilustracion} alt="construction"/>
+            <Image priority src={ilustracion} alt=""/>
             </div>
         </div>
     )
