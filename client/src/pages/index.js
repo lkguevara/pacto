8a0807f4f31cfd84d614f767c373de96f11f392f
Alexbv2615 <alexbv2615@gmail.com>
2023-04-27 19:55:09 -0500

    Slider responsive terminado

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,13 +3,15 @@ import Layout from '@/components/layout';
 import Link from 'next/link';
 import Login from '@/pages/login';
 import EnConstruccion from '@/components/enConstruccion';
+import Slider from '@/components/slider';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Home() {
   return (
     <>
       <Layout title="Home">
-        <EnConstruccion />
+        <Slider />
       </Layout>
     </>
   )
