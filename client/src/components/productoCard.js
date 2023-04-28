f94088640736f3f0f0a11bfa69226b9fcb904e7f
MartaFagundez <martafagundez.dev@gmail.com>
2023-04-28 09:55:23 -0300

    refactor/productoCard ajustes de estilo

--- client/src/components/productoCard.js
+++ client/src/components/productoCard.js
@@ -1,8 +1,10 @@
 import styles from "../styles/ProductoCard.module.css";
 import Image from "next/image";
+import Link from "next/link";
 import img1 from '../../public/image/products/p1_1.jpg';
 
 const hardProduct = {name: "Iphone 20", price: 125000, images: [img1]};
+
 
 const ProductoCard = ({ producto = hardProduct }) => {
 
@@ -15,6 +17,10 @@ const ProductoCard = ({ producto = hardProduct }) => {
                     <p>{producto.name}</p>
                 </div>
                 <p>{`$ ${producto.price}`}</p>
+                {/* HARDCODEO DE LINK PARA EL BORRADOR INICIAL */}
+                <Link href={"/"} className={styles.link}>
+                    Ver Producto
+                </Link>
             </div>
         </div>
     )
