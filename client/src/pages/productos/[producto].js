1c58e54e1509503c7d84be287f7c929c405f9d6d
Lian Katherin Guevara <liann.guevara@gmail.com>
2023-04-30 11:12:55 -0500

    detail product

--- client/src/pages/productos/[producto].js
+++ client/src/pages/productos/[producto].js
@@ -10,37 +10,40 @@ function producto() {
     const { producto } = router.query;
   return (
     <Layout>
-        <h1 style={style.detailProduct}> {producto}</h1>
+      <div className={style.containerDetail}>
+
+        <h2 className={style.detailTitle}> {producto}</h2>
         
-        <div className="informationProduct">
+        <div className={style.informationProduct}>
             <Image className= {style.imageProduct} priority src="/image/products/p1_1.jpg" alt="logo" width="150" height="150"/>
             <div className={style.infoProduct}>
                 <h3>{producto}</h3>
 
-                <div className="price">
+                <div className={style.price}>
                   <h4>$ 3.800.000 </h4>
-                  <button>Ofertar</button>
+                  <button className={style.priceButton}>Ofertar</button>
                 </div>
 
-                <p>Gastos de envío calculados al momento de realizar tu compra.</p>
-                <div className="information">
-                  <div className="containerInfo">
-                    <Image className= {style.truck} priority src="/image/truck.png" alt="truck" width="15" height="15"/>
+                <p className={style.gastoEnvio}>Gastos de envío calculados al momento de realizar tu compra.</p>
+
+                <div className={style.informationAditional}>
+                  <div className={style.containerInfo}>
+                    <Image className= {style.truck} priority src="/image/truck.png" alt="truck" width="20" height="20"/>
                     <p>Envío: <span>NACIONAL</span></p>
                   </div>
-                  <div className="containerInfo">
-                    <Image className= {style.truck} priority src="/image/devolucion.png" alt="devolucion" width="15" height="15"/>
+                  <div className={style.containerInfo}>
+                    <Image className= {style.truck} priority src="/image/devolucion.png" alt="devolucion" width="20" height="20"/>
                     <p>Devolución: <span>GRATIS</span></p>
                   </div>
-                  <div className="containerInfo">
-                    <Image className= {style.truck} priority src="/image/garantia.png" alt="garantia" width="15" height="15"/>
+                  <div className={style.containerInfo}>
+                    <Image className= {style.truck} priority src="/image/garantia.png" alt="garantia" width="20" height="20"/>
                     <p>Garantía: <span>06 MESES</span></p>
                   </div>
                 </div>
                 
-                <p>Puedes recibir tu pedido al día siguiente de tu compra. Conoce el tiempo exacto, una vez confirmes la dirección de entrega</p>
+                <p className={style.infoEnvio}>Puedes recibir tu pedido al día siguiente de tu compra. Conoce el tiempo exacto, una vez confirmes la dirección de entrega</p>
 
-                <div className="stock">
+                <div className={style.stock}>
                   <p>Stock: <span>Disponible</span></p>
                   <label>Cantidad</label>
                   <input type="number" min="1" max="4" placeholder="1"/>
@@ -48,34 +51,35 @@ function producto() {
                   <p>Información del vendedor ℹ️</p>
                 </div>
 
-                <div className="buttons">
-                  <button>Agregar al carrito</button>
-                  <button>Comprar ahora</button>
+                <div className={style.buttons}>
+                  <button className={style.buttonAdd}>Agregar al carrito</button>
+                  <button className={style.buttonSell}>Comprar ahora</button>
                 </div>
             </div>
         </div>
 
-        <div className="descriptionProduct">
-          <h3>Descripción</h3>
+        <h3>Descripción del producto</h3>
+        <div className={style.descriptionProduct}>
           <p>El Samsung Galaxy Xcover 4s es un smartphone Android con certificación IP68 que lo hace resistente al agua y al polvo. Entre sus características se destacan una pantalla HD de 5 pulgadas, procesador Exynos 7885 de ocho núcleos con 3GB de RAM y 32GB de almacenamiento interno, cámara principal de 16 MP, cámara frontal de 5 MP, batería de 2800 mAh, lector de huellas dactilares, y corre Android 9.0 Pie.</p>
         </div>
 
         <div className="preguntasRespuestas">
           <h3>Preguntas y respuestas</h3>
-          <div className="containerPregunta">
+          <div className={style.containerPregunta}>
             <h4>Preguntale al vendedor</h4>
             <input type="text" placeholder="Escribe tu pregunta"/>
-            <button>Pregntar</button>
+            <button className={style.buttonAsk}>Pregntar</button>
           </div>
 
           <h4>Últimas realizadas</h4>
           <div className="containerPreguntas">
-            <div className="pregunta">
-              <p>¿El producto es nuevo?</p>
+            <div className={style.ask}>
+              <span>¿El producto es nuevo?</span>
               <p>Si, el producto es nuevo</p>
             </div>
           </div>
         </div>
+      </div>
             
 
     </Layout>
