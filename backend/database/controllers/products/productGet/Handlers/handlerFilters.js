826c8ce63c71015e322bb6efbd3ff3222a1998aa
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-05-03 13:26:00 -0300

    Modified the cases of the switch in DBProductsGetFilters, was commented one case too

--- backend/database/controllers/products/productGet/Handlers/handlerFilters.js
+++ backend/database/controllers/products/productGet/Handlers/handlerFilters.js
@@ -9,10 +9,10 @@ const handlerFilters = async (filters) => {
         searchproduct.where({"category" : filters.categories.category})
         if (filters.categories.subcategory !== null) searchproduct.where({"subcategory" : filters.categories.subcategory})
     }
-    if (filters.status) searchproduct.or({"state" : filters.status})
-    if (filters.price){
-        if (filters.price.min) searchproduct.where("price").gte(filters.price.min)
-        if (filters.price.max) searchproduct.where("price").lte(filters.price.max)
+    if (filters.status !== null) searchproduct.or({"state" : filters.status})
+    if (filters.price !== null){
+        if (filters.price.min !== null) searchproduct.where("price").gte(filters.price.min)
+        if (filters.price.max !== null) searchproduct.where("price").lte(filters.price.max)
     }
     
     const products = await searchproduct.exec()
