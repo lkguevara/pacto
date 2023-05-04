9c1dc1b13d243a1aa7dee8ba9c4d029ad9254446
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-04 16:00:06 -0300

    refactor/productCard: ahora muestra la imagen del producto

--- client/src/components/productoCard.js
+++ client/src/components/productoCard.js
@@ -7,10 +7,13 @@ const hardProduct = {name: "Iphone 20", price: 125000, images: [img1]};
 
 
 const ProductoCard = ({ producto = hardProduct }) => {
-
+    
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
+                <Link href={`productos/${producto.name}`} >
+                    <Image src={producto.images[0]} alt={producto.name} className={styles.img} width="200" height="200" />
+                </Link>
             </div>
             <div className={styles.infoContainer}>
                 <div className={styles.nameContainer}>
