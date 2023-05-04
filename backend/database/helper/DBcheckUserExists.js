a904c5ef2878829e02d1a71c4089549ee41c0784
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-05-04 13:17:29 -0300

    ADD: Now you can check if a user exists by their email

--- backend/database/helper/DBcheckUserExists.js
+++ backend/database/helper/DBcheckUserExists.js
@@ -2,9 +2,17 @@ const  User  = require("../models/user")
 
 //Funcion para verificar si el usuario existe en la base de datos
 
-const checkUserExists = async (idUser) => {
-    const isUser = await User.findById(idUser)
-    if (isUser !== null) {
+const checkUserExists = async (idUser, email = false) => {
+    let isUser = null;
+    if (email){
+        isUser = await User.findOne({email: email})
+    } else{
+        isUser = await User.findById(idUser);
+    }
+
+
+    if (isUser != null) {
+
         return true
     }
 
