edd110944b5224ea48f24dd35d98583fd2b83c32
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-05 01:30:22 -0300

    Ajustados links de las tarjetas de destacados

--- client/src/components/destacadoCard.js
+++ client/src/components/destacadoCard.js
@@ -7,19 +7,19 @@ const DestacadoCard = ({ producto }) => {
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
-                <Link href={`productos/${producto.name}`}>
+                <Link href={`productos/${producto._id}`}>
                     <Image src={producto.images[0]} alt={producto.name} width="400" height="285"/>
                 </Link>
             </div>
             <div className={styles.infoContainer}>
                 <div className={styles.nameContainer}>
-                    <Link href={`productos/${producto.name}`} className={styles.link}>
+                    <Link href={`productos/${producto._id}`} className={styles.link}>
                         <p>{producto.name}</p>
                     </Link>
                 </div>
                 <p>{`$ ${producto.price}`}</p>
             </div>
-            <Link href={`productos/${producto.name}`}>
+            <Link href={`productos/${producto._id}`}>
                 <button className={styles.buyNowButton} title="Ver Producto">
                     Ver Producto
                 </button>  
