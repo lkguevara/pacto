9728e0b0e3e0bcc6b9d7878d68d104d7f5318ec9
Edward Raga <edwardraga@icloud.com>
2023-05-06 23:51:21 -0400

    feat(signup) : add logic for register a new user

--- client/src/redux/features/auth/authSlice.js
+++ client/src/redux/features/auth/authSlice.js
@@ -1,45 +1,76 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-    msg:'',
-    user:'',
-    token:'',
-    loading:false,
-    error:''
-}
+  user: null,
+  sendCode: false,
+  token: null,
+  loading: false,
+  error: null,
+  verify:false,
+};
 
-export const signUpUser = createAsyncThunk('signupuser',async(body)=>{
-   const respuesta =  await axios.post("http://localhost:3000/api/signup",body);
-   return respuesta.data
-})
+// Async thunk para enviar los datos del usuario al backend y recibir la respuesta con el token y código de verificación
+export const registerUser = createAsyncThunk(
+  'auth/registerUser',
+  async (userData) => {
+    console.log(userData);
+    const response = await axios.post('http://localhost:3001/user', userData);
+    return response.data;
+  }
+);
 
-const authSlice = createSlice({
-    name:"auth",
-    initialState,
-    reducers:{
-      
-    },
-    //reducer async
-    extraReducers:{
-        [signUpUser.pending]: (state,action)=>{
-            state.loading = true
-        },
-        [signUpUser.fulfilled]: (state,{payload:{error,msg}})=>{
-            state.loading = false;
-            if(error){
-                state.error = error
-            }else{
-                state.msg = msg
-            }
-        },
-        [signUpUser.rejected]: (state,action)=>{
-            state.loading = true
-            
-        }
+// Async thunk para enviar el código de verificación al backend y recibir la respuesta con los datos del usuario
+export const verifyCode = createAsyncThunk(
+    'auth/fetchUserData',
+    async ({ email, code,token}) => {
+      console.log(token);
+      const response = await axios.get(`http://localhost:3001/verify?email=${email}&code=${Number(code)}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      return response.data;
     }
-})
-
-
-// export const {addToken,addUser,logout} = authSlice.actions
+  );
+  
+  const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        // Acciones para enviar los datos del usuario y recibir el token y código de verificación
+        .addCase(registerUser.pending, (state) => {
+          state.loading = true;
+        })
+        .addCase(registerUser.fulfilled, (state, action) => {
+          state.loading = false;
+          state.sendCode = true;
+          state.token = action.payload.token;
+          localStorage.setItem('token', action.payload.token);
+        })
+        .addCase(registerUser.rejected, (state, action) => {
+          state.loading = false;
+          state.error = action.payload;
+        })
+        // Acciones para enviar el código de verificación y recibir los datos del usuario
+        .addCase(verifyCode.pending, (state) => {
+          state.loading = true;
+        })
+        .addCase(verifyCode.fulfilled, (state, action) => {
+          state.loading = false;
+          state.sendCode = false;
+          state.user = action.payload.user;
+          state.token = action.payload.token;
+          state.verify = true;
+          localStorage.setItem('token', action.payload.token);
+        })
+        .addCase(verifyCode.rejected, (state, action) => {
+          console.log(action);
+          state.loading = false;
+          state.error = action.payload;
+          state.sendCode = true
+        })
+    },
+  });
+  
 export default authSlice.reducer;
