5f366064c70c865c2f6e25d6e289649e886c7960
sumerlost <Gaby_98@hotmail.com.ar>
2023-05-06 22:27:37 -0300

    uninstall firebase

--- backend/app.js
+++ backend/app.js
@@ -6,30 +6,6 @@ const logger = require('morgan');
 const cors = require('cors');
 const send = require('./controllers/utils/emailService/singnUp')
 require('dotenv').config()
-const { getAuth, onAuthStateChanged } = require("firebase/auth")
-const { initializeApp } = require("firebase/app")
-const { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } = process.env
-// Import the functions you need from the SDKs you need
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-
-//Firebase Config
-const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const firebaseAuth = getAuth(firebaseApp)
-const user = firebaseAuth.currentUser
-console.log(onAuthStateChanged)
 
 //RUTAS!!!
 const indexRouter = require('./routes/index');
