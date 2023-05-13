c2445e82b53e3fd82f8924b6e835d7bc513713c8
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-13 15:15:43 -0300

    adminDash/nuevas páginas para las diferentes secciones

---
+++ client/src/pages/dashboard/admin/ventas.js
@@ -0,0 +1,12 @@
+import Layout from '@/components/dashboard/admin/Layout.js';
+
+export default function Ventas() {
+
+  return (
+    <>
+      <Layout title="Ventas">
+        <h1>Ventas</h1>
+      </Layout>
+    </>
+  )
+}
