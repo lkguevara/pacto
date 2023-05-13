4b5326325df61a76f9336833e306c4c5c56316ff
sumerlost <Gaby_98@hotmail.com.ar>
2023-05-13 03:31:12 -0300

    Wallet updated

---
+++ backend/database/schemas/orderSchema.js
@@ -0,0 +1,23 @@
+const { Schema } = require("mongoose");
+const boughtProductSchema = Schema({
+    product: { type: Schema.Types.ObjectId, ref: "Product" },
+    ammount: Number,
+    state: {
+        type: String,
+        enum: ["comprado", "entregado"],
+        default: "comprado"
+    }
+})
+
+const orderSchema = Schema({
+    id: Schema.Types.ObjectId,
+    user: { type: Schema.Types.ObjectId, ref: "User" },
+    products: [boughtProductSchema],
+    state: {
+        type: String,
+        enum: ["en curso", "finalizada"],
+        default: "en curso"
+    }
+})
+
+module.exports = orderSchema
