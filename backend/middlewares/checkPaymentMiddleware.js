a8bf8539dc7f0bf7571eafa994c00d8ba0545234
Edward Raga <edwardraga@icloud.com>
2023-05-13 17:43:30 -0400

    feat(backend/ middleware) :add for formatting data of products to send api mercado pago

--- backend/middlewares/checkPaymentMiddleware.js
+++ backend/middlewares/checkPaymentMiddleware.js
@@ -1,8 +1,53 @@
+const checkUserExists = require("../database/helper/DBcheckUserExists")
+const checkProductExists = require("../database/helper/DBCheckProductExists");
 const checkProductsPayment = async(req,res,next) =>{
-    const user = req.userId
-    //vallidaciones antes de enviar a mercado pago
 
-    next()
+    //token usuario
+    const userId = req.userId;
+    //productos del shopping cart del usuario
+    const shoppingCart  = req.body;
+    //validar el usuario
+    const user = await checkUserExists(userId,null);
+
+
+    //productos con formato mercado pago
+    const formattingProducts = [];
+
+
+    if(user){
+
+        const productsMercadoPago = Promise.all(shoppingCart.map(async(item)=>{
+
+            //validar existencia del producto
+            const realProduct = await checkProductExists(item.product._id)
+
+            if(realProduct){
+
+                const productMercadoPago = {
+                    id: item.product._id,
+                    title: item.product.name,
+                    currency_id: "COP",
+                    description: item.product.description.slice(0,255),
+                    picture_url: item.product.images[0],
+                    category_id: item.product.category,
+                    quantity: item.ammount,
+                    unit_price: item.product.price
+                } 
+    
+                formattingProducts.push(productMercadoPago)
+            }
+
+            
+        })) 
+
+        await productsMercadoPago;
+
+    }
+    // console.log(formattingProducts);
+    //enviar data al controller
+    req.body = {emailUser: user.email,items:formattingProducts}
+
+    next();
 
 }
 
