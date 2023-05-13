59ed30ebab470ff984abab804477f64e6d9f69c1
Edward Raga <edwardraga@icloud.com>
2023-05-13 17:39:31 -0400

    refactor: show real data of products

--- client/src/pages/cart/detailCart.js
+++ client/src/pages/cart/detailCart.js
@@ -4,26 +4,38 @@ import Link from "next/link";
 import style from "../../styles/cart/car.module.css";
 import Image from "next/image";
 import ProductoCard from "../../components/cart/productCart";
+import { useSelector } from "react-redux";
 
 export default function car() {
+
+  const shoppingCart = useSelector((state)=> state.shoppingCart.products);
+  let totalPrice = null;
+
   return (
     <>
       <Layout>
         <div className={style.container}>
             <p className={style.titleCart}>Carrito de compras</p><hr/>
 
-            <ProductoCard />
+            {shoppingCart.length > 0 && shoppingCart.map(item=>{
+              const {product,ammount,_id} = item;
+
+              totalPrice += product.price;
+            
+              return (<ProductoCard name= {product.name} images={product.images} price={product.price} ammount={ammount}/>)
+            })}
+
 
             {/*  Costo envío*/}
-            <div className={style.shipping}>
+            {/* <div className={style.shipping}>
               <h5>Costo de envío</h5>
               <p>$500</p>
-            </div>
+            </div> */}
 
-            {/*  total producto*/}
-            <div className={style.shipping}>
+             {/* total producto */}
+             <div className={style.shipping}>
               <h5>Total con envío</h5>
-              <p>$ 1500</p>
+              <p>{`$ ${totalPrice}`}</p>
             </div>
             
             {/* Sección para los botones de submit y cancelar */}
