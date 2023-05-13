c3de8c759ed36d2ef67f4f4b764585d658ca1f41
sumerlost <Gaby_98@hotmail.com.ar>
2023-05-12 23:44:34 -0300

    fixedadas rutas de controllers

--- backend/routes/shoppingCart.js
+++ backend/routes/shoppingCart.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { verifyToken } = require('../controllers/token/verifyToken');
-const  shoppingCart  = require('../controllers/shoppingCart/shoppingCart.controller') 
-const getShoppingCart = require('../controllers/shoppingCart/getShoppingCart.controller')
+const shoppingCart = require('../controllers/shoppingCart/shoppingCart.controller')
+const getShoppingCart = require('../controllers/shoppingCart/shoppingCart.controller')
 
 
 //Add products at shopping cart user
-router.post('/shoppingcart',verifyToken, (req,res)=> shoppingCart(req,res))
+router.post('/shoppingcart', verifyToken, (req, res) => shoppingCart(req, res))
 //Add products at shopping cart user
-router.get('/shoppingcart',verifyToken, (req,res)=> getShoppingCart(req,res))
+router.get('/shoppingcart', verifyToken, (req, res) => getShoppingCart(req, res))
 
 module.exports = router;
