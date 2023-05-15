f2e8cb97fd6c3c89a3c4dc8599629473e4a16e57
Alexbv2615 <alexbv2615@gmail.com>
2023-05-15 15:25:58 -0500

    fix

--- backend/controllers/product/APIpostNewProduct.js
+++ backend/controllers/product/APIpostNewProduct.js
@@ -7,13 +7,13 @@ const postNewProduct = async (req, res) => {
 
 
         //producto con todos los datos
-        const { newProduct } = req;
+        const { newProduct } = req.body;
 
-        
+   
   
 
         if (idUser && newProduct){
-            const postedProd = await productSave(newProduct, idUser);
+            const postedProd = await productSave(newProduct, newProduct.idUser);
   
             //Checkeo que el producto fue creado en la db
             if (checkProductExists(postedProd._id.toString())){
