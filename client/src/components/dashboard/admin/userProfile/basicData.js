ba7ea36e5f8c6667cd23399a53767f88899f0b8f
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-15 01:45:59 -0300

    Creadas rutas para el detalle del usuario, del producto y de la venta

---
+++ client/src/components/dashboard/admin/userProfile/basicData.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BsPersonFill } from 'react-icons/bs';
+
+function BasicData({ name, lastname, status, verified, colSpan="col-span-1"}) {
+  return (
+    <div className={`${colSpan} bg-white flex justify-between w-full h-fit border p-4 rounded-lg`} >
+        {/* Ícono de usuario */}
+        <div className="p-3 rounded-full bg-green-100  " >
+            <BsPersonFill className='text-4xl text-verde' />
+        </div>
+    </div>
+  )
+};
+
+export default BasicData;
