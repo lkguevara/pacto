d107e26f075749b83b534b9772ca833e151f0fb3
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-05-16 00:23:59 -0300

    Restructured code of checkUserExists, maybe new file needs to be created

--- backend/database/helper/DBcheckUserExists.js
+++ backend/database/helper/DBcheckUserExists.js
@@ -4,72 +4,50 @@ const User = require("../models/user")
 /* Se agregó el parametro lean t/f ya que en algunos casos del proyecto se necesita que se retorne 
 un json plano (lean true) y en otro que retorne el json con las funciones de mongoose (lean false)*/
 const checkUserExists = async (idUser, email = false, lean = false) => {
-    let isUser = null;
-    if (email) {
-        if (lean) {
-            isUser = await User.findOne({ email: email })
-                .populate('role', 'role')
-                .populate('city', 'city')
-                .populate({
-                    path: 'city',
-                    populate: {
-                        path: 'department',
-                        select: 'department -_id'
-                        // Selecciona el campo 'department' del departamento
-                    }
-                })
-                .lean();
+
+    try {
+        let isUser = null;
+        if (email) {
+            
+            isUser = User.findOne({ email: email });
+          
         } else {
-            isUser = await User.findOne({ email: email })
-                .populate('role', 'role')
-                .populate('city', 'city')
-                .populate({
-                    path: 'city',
-                    populate: {
-                        path: 'department',
-                        select: 'department -_id'
-                        // Selecciona el campo 'department' del departamento
-                    }
-                });
+            
+            isUser = User.findById(idUser);
+    
         }
-
-    } else {
-        if (lean) {
-            isUser = await User.findById(idUser)
-                .populate('role', 'role')
-                .populate('city', 'city')
-                .populate({
-                    path: 'city',
-                    populate: {
-                        path: 'department',
-                        select: 'department -_id'
-                        // Selecciona el campo 'department' del departamento
-                    }
-                })
-                .lean();
-        } else {
-            isUser = await User.findById(idUser)
-                .populate('role', 'role')
-                .populate('city', 'city')
-                .populate({
-                    path: 'city',
-                    populate: {
-                        path: 'department',
-                        select: 'department -_id'
-                        // Selecciona el campo 'department' del departamento
-                    }
-                });
+    
+        if (isUser != null) {
+    
+            isUser = isUser.populate('role', 'role')
+            .populate('city', 'city')
+            .populate({
+                path: 'city',
+                populate: {
+                    path: 'department',
+                    select: 'department -_id'
+                    // Selecciona el campo 'department' del departamento
+                }
+            })
+        
+            if (lean){
+               
+                isUser.lean();
+            }
+    
+            isUser = await isUser
+    
+            return isUser
         }
+    
+       
+    
+        return false;
 
-
+    } catch (err){
+        return false;
     }
-
-    if (isUser != null) {
-
-        return isUser
-    }
-
-    return false
+    
 }
 
 
