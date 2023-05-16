79a95d12f20bdaffc5e79b16ba903b933227322b
MartaFagundez <martafagundez.dev@gmail.com>
2023-05-16 11:42:39 -0300

    Bloquear producto casi terminado (falta ruta en el back)

--- client/src/pages/dashboard/admin/productos/[id].js
+++ client/src/pages/dashboard/admin/productos/[id].js
@@ -1,11 +1,26 @@
 import Layout from '@/components/dashboard/admin/Layout.js';
 import EnConstruccion from '../../../../components/enConstruccion';
+import { useRouter } from 'next/router';
+
 
 function Producto() {
+    // LÓGICA DEL COMPONENTE
+    const router = useRouter();
+
+    // RENDERIZADO DEL COMPONENTE
     return (
         <Layout title="Producto">
             <div className='mt-8'>
                 <EnConstruccion />
+            </div>
+            <div className='flex justify-center items-center'>
+                <button 
+                className='bg-verde hover:bg-verde-light text-white font-semibold text-sm py-2 px-4 rounded mt-8'
+                type="button" 
+                onClick={() => router.back()}
+                >
+                    Volver
+                </button>
             </div>
         </Layout>
     )
